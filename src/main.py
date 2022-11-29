26c99668f7bcf0ec42817a70f974470fab3b725b
Alireza Goudarzi <alireza.goudarzi@gmail.com>
2022-11-29 07:10:51 +0000

    check ui

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-""" A test file.
 
-"""
+
+import kivy
+kivy.require('2.1.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
 
 def hello_function():
-    """_summary_
-    """
-    print("Hello Function")
+    print('returning 42')
+    return 42
+    
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
 
 if __name__ == '__main__':
-    hello_function()
-    
+    MyApp().run()
+
+
