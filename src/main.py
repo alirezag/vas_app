fc4fd0e686bcc010cd12122a60c76e319f3b7fd1
Alireza Goudarzi <alireza.goudarzi@gmail.com>
2022-11-30 06:43:22 +0000

    add server into main.py

--- src/main.py
+++ src/main.py
@@ -4,12 +4,58 @@ import webbrowser
 from subprocess import Popen
 import json
 import argparse
-from .server import setup_server
+import http.server
+import socketserver
+from multiprocessing import Process
+import json
 
 config = json.load(open('config.json'))
 
 def hello_function():
     print('Hello Function')
+
+config = json.load(open('config.json'))
+PORT=config.get('server_port',8000)
+
+def setup_server():
+    class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+        def do_GET(self):
+            # self.path = os.path.join(config.get('app_dir','./app'),self.path)
+            # self.path = "./app/"+self.path
+            self.path = config.get('app_dir','./app') + self.path
+            super().do_GET()
+           
+
+        def do_POST(self): 
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len)
+            print(json.loads(post_body.decode()))
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+
+            output = json.dumps({"result" : post_body.decode()})
+            self.wfile.write(output.encode())
+            print(output)
+            return
+
+          
+
+            
+
+
+
+
+
+    Handler = MyHttpRequestHandler
+
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
 
 def open_url():
     webbrowser.open(f'http://localhost:{config.get("server_port",8000)}', new=0, autoraise=True)
